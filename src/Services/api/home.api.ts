90165d9ab85bfdf1245c239ab3569db36ad0dacd
Thermiee <navyblack16@yahoo.com>
2025-04-26 00:17:39 +0100

    update https

--- src/Services/api/home.api.ts
+++ src/Services/api/home.api.ts
@@ -1,17 +1,20 @@
-
 import axiosInstance from "../../Utils/Https";
 
-
 const apiGetRequest = async (endpoint: string) => {
-    try {
-      const response = await axiosInstance.get(endpoint);
-      return response.data;
-    } catch (error: any) {
-      throw error.response?.data || error;
-    }
+  try {
+    const response = await axiosInstance.get(endpoint);
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error;
   }
+};
 
+// Fetch Competition Data (e.g., Premier League)
+export const getCompetition = (competitionSlug: string) => {
+  return apiGetRequest(`/video-api/v3/competition/${competitionSlug}/`);
+};
 
-  export const getTeamDetails = async (title: string) => {
-    return apiGetRequest(`${title}`);
-  }
+// Fetch Team Data (e.g., Real Madrid)
+export const getTeam = (teamSlug: string) => {
+  return apiGetRequest(`/video-api/v3/team/${teamSlug}/`);
+};
